6cc4e219625d99761174a26804674012f6b09b23
vishnu venu <vishnuvenuvenk@gmail.com>
2019-09-16 11:20:29 +0530

    changed token to body 2

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def presign_url():
 
     req = request.json
     file_data = req["file_data"].split("/")
-    jwt_token = request["token"]
+    jwt_token = req["token"]
     # return jwt_token
 
     try:
